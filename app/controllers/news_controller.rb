ce547248b40ba9cc8535b1c45ec477d7b062d80f
Sergey Belotelkin <serwhite@yandex.ru>
2013-02-12 12:21:51 +0400

    change constant WHITE to WHITEKIT

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,7 +2,7 @@ class NewsController < ApplicationController
 
   # All news
   def index
-    @news = News.visible.page(params[:page]).per(WHITE[:news_per_page])
+    @news = News.visible.page(params[:page]).per(WHITEKIT[:news_per_page] || 10)
   end
 
   # Single news
