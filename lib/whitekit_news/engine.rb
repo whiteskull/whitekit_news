8b8b7fbd45bb53c3de6b70bbdbca75ae252ccac9
Sergey Belotelkin <serwhite@yandex.ru>
2013-02-13 11:53:54 +0400

    css and js add to precompile

--- lib/whitekit_news/engine.rb
+++ lib/whitekit_news/engine.rb
@@ -1,5 +1,8 @@
 module WhitekitNews
   class Engine < Rails::Engine
+    initializer "css and js precompile" do |app|
+      app.config.assets.precompile += %w(news.js news.css)
+    end
   end
 end
 
